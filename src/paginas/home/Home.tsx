3a6a6a7e2c4a3f8818c4c65b39f452d485d41ac9
victoriamelo09 <vitoria.melo0901@hotmail.com>
2022-10-27 20:33:56 -0300

    commit inicial

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css'; 
+
+function Home(){
+    return (
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://i.imgur.com/AD3MbBi.jpeg" alt="Imagem Tela Inicial" className="img" />
+        </>
+
+    ); 
+}
+
+export default Home; 
