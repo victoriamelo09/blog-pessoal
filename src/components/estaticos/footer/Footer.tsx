5002a1f033d5174eb7b4eba70c1c38d2cc99d8da
victoriamelo09 <vitoria.melo0901@hotmail.com>
2022-11-02 00:21:34 -0300

    adicionando o conteudo dos estilos

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -4,6 +4,7 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Grid } from '@material-ui/core';
 import {Box} from '@mui/material'
+import './Footer.css';
 
 function Footer (){
     return (
@@ -11,29 +12,29 @@ function Footer (){
         <>
         <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#5F3534", height: "120px" }}>
+                    <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/victoriamelo09" target="_blank">
-                                <GitHubIcon style={{ fontSize: 40, color: "white" }} />
+                                <GitHubIcon  className='redes'/>
                             </a>
                             <a href="https://www.instagram.com/victoriamello_c/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 40, color: "white" }} />
+                                <InstagramIcon className='redes'/>
                             </a>
                             <a href="https://www.linkedin.com/in/victória-melo-a143b9171/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 40, color: "white" }} />
+                                <LinkedInIcon className='redes'/>
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos'>© 2020 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
